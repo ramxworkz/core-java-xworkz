24cff07a5fd0a7a158e1d74803c5ae892b764288
ramxworkz <mramkumar.xworkz@gmail.com>
2023-04-20 11:32:16 +0530

    adding methods related java files

---
+++ Hotel.java
@@ -0,0 +1,5 @@
+class Hotel {
+         public static void main(String Rk[]){
+            System.out.println("food...was very taste");
+         }
+  }
---
+++ Television.java
@@ -0,0 +1,9 @@
+class Television {
+      public static void main(String a[]){
+         String   brandName = "onida";
+         int  price = 7000;
+         System.out.println("The Television brand name is "+brandName);
+         System.out.println("The Television price is "+price);
+      }
+
+}
