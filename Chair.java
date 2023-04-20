e58026c5b11cbc1267e5e754828d6bd7af134a0c
ramxworkz <mramkumar.xworkz@gmail.com>
2023-04-20 21:35:56 +0530

    syntax related java files

---
+++ Chair.java
@@ -0,0 +1,5 @@
+class Chair {
+       public static void main(String ram[]){
+          System.out.println("Useful...for sitting");
+       }
+   }
--- Television.java
+++
@@ -1,9 +0,0 @@
-class Television {
-      public static void main(String a[]){
-         String   brandName = "onida";
-         int  price = 7000;
-         System.out.println("The Television brand name is "+brandName);
-         System.out.println("The Television price is "+price);
-      }
-
-}
